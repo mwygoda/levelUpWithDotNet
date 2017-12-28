46e0b2060b83eabf0b65bd73cdef8b1299d503e4
mwygoda <mwygoda_94@o2.pl>
2017-12-29 00:19:54 +0100

    Created basic consol app with EF

---
+++ EFTransaction/EFTransaction/Model.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace EFTransaction
+{
+    public class PostsContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ EFTransaction/EFTransaction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EFTransaction;
+
+namespace EFTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new PostsContext())
+            {
+                db.Posts.Add(new Post { Name = "New Post", Content = "Lorem Ipsum..."});
+                db.SaveChanges();
+
+                foreach (var post in db.Posts)
+                {
+                    Console.WriteLine(post.Name);
+                }
+            }
+            Console.WriteLine("Press any button to continue...");
+            Console.ReadKey();
+        }
+    }
+}
