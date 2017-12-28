8a93fd31dac5a3f0c4fa7b5dc9f1f4614903c91d
mwygoda <mwygoda_94@o2.pl>
2017-12-29 00:44:02 +0100

    Changed example to using transaction

--- EFTransaction/EFTransaction/Program.cs
+++ EFTransaction/EFTransaction/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using EFTransaction;
 
 namespace EFTransaction
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using (var db = new PostsContext())
             {
-                db.Posts.Add(new Post { Name = "New Post", Content = "Lorem Ipsum..."});
-                db.SaveChanges();
+                using (var dbTransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Posts.Add(new Post {Name = "New Post about EF", Content = "Lorem Ipsum..."});
+                        db.SaveChanges();
+                        db.Posts.Add(new Post {Name = "Transaction using EF", Content = "Works like a dream"});
+                        db.SaveChanges();
+
+                        dbTransaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        dbTransaction.Rollback();
+                        Console.WriteLine(e);
+                    }
+                }
 
                 foreach (var post in db.Posts)
-                {
                     Console.WriteLine(post.Name);
-                }
             }
             Console.WriteLine("Press any button to continue...");
             Console.ReadKey();
