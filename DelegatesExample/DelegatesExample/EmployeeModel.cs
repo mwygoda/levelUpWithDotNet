92207a5c75410040150610b59b7f4a4464749226
mwygoda <mwygoda_94@o2.pl>
2017-12-31 11:33:39 +0100

    Created project structure and basic idea of delegates example

---
+++ DelegatesExample/DelegatesExample/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Profession { get; set; }
+        public int Age { get; set; }
+    }
+}
