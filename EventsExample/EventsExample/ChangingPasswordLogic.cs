88c0a3dc9a01cbae1ac58b0b00689c0a51bf1ece
mwygoda <mwygoda_94@o2.pl>
2018-01-01 20:44:49 +0100

    Example with event works without passing any data

---
+++ EventsExample/EventsExample/ChangingPasswordLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsExample
+{
+    public class ChangingPasswordLogic
+    {
+        public delegate void ChangingPasswordEventHandler(object source, EventArgs args);
+        public event ChangingPasswordEventHandler PasswordChanged;
+
+        public void ProcessPasswordChange(Credentials credentials)
+        {
+            Console.WriteLine("Processing and verifing new credentials...");
+            Thread.Sleep(5000);
+
+            OnPasswordChanged();
+        }
+
+        protected virtual void OnPasswordChanged()
+        {
+            if (PasswordChanged != null)
+            {
+                PasswordChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ EventsExample/EventsExample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentials = new Credentials()
+            {
+                Username = "Mateuszek",
+                Password = "Qwerty@123"
+            };
+            var changingPasswordProcessor = new ChangingPasswordLogic();
+            var mailService = new EmailService();
+            var smsService = new SmsService();
+            var postmanService = new PostmanService();
+
+            changingPasswordProcessor.PasswordChanged += mailService.OnPasswordChanged;
+            changingPasswordProcessor.PasswordChanged += smsService.OnPasswordChanged;
+            changingPasswordProcessor.PasswordChanged += postmanService.OnPasswordChanged;
+
+            changingPasswordProcessor.ProcessPasswordChange(credentials);
+
+            Console.ReadLine();
+        }
+
+        public class EmailService
+        {
+            public void OnPasswordChanged(object source, EventArgs args)
+            {
+                Console.WriteLine("Sending an email...");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public class SmsService
+        {
+            public void OnPasswordChanged(object source, EventArgs args)
+            {
+                Console.WriteLine("Sending text message on users cellphone..");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public class PostmanService
+        {
+            public void OnPasswordChanged(object source, EventArgs args)
+            {
+                Console.WriteLine("Ordering postman to deliver parcel with confirmation..");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
