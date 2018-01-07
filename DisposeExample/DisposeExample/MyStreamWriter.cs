6c10d7e605201e59be818d3da6fa216e43e1a826
mwygoda <mwygoda_94@o2.pl>
2018-01-07 12:41:47 +0100

    Finished implementing IDisposable example

---
+++ DisposeExample/DisposeExample/MyStreamWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeExample
+{
+    public class MyStreamWriter : IDisposable
+    {
+        private readonly StreamWriter _streamWriter;
+
+        public MyStreamWriter(string path)
+        {
+            _streamWriter = new StreamWriter(path);
+        }
+
+        public void WriteTextToFile(string line)
+        {
+            _streamWriter.WriteLine(line);
+        }
+
+    protected virtual void Dispose(bool b)
+        {
+            if (b)
+            {
+                _streamWriter.Dispose();
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ DisposeExample/DisposeExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = {"This is LoremIpsumLine1", "This is LoremIpsumLine2", "This is LoremIpsumLine3", "This is LoremIpsumLine4" };
+            const string path = @"name.txt";
+
+            var writer = new MyStreamWriter(path);
+            try
+            {
+                foreach (var line in lines)
+                {
+                    writer.WriteTextToFile(line);
+                }
+                writer.Dispose();
+
+                var fileContent = ReadResult(path);
+                Console.WriteLine(fileContent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string ReadResult(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                var result = reader.ReadToEnd();
+
+                return result;
+            }
+        }
+    }
+}
